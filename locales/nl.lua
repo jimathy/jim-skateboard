e6f26195e4564f1b8dd6e6538caea156b4acec2b
MriKNG <ayrton.buyle@hotmail.com>
2026-05-31 12:28:40 +0200

    Added dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,31 @@
+Loc = Loc or {}
+
+Loc["nl"] = {
+    info = {
+        getoff = "Stap van het skateboard",
+        getoffSurf = "Stap van het surfboard",
+        lockcam = "Camera vergrendelen",
+        jump = "Springen",
+        putDown = "[Backspace] Board neerzetten"
+    },
+    keyMaps = {
+        lockCam = "Skateboard: Camera vergrendelen/ontgrendelen",
+        flipCam = "Skateboard: Camera draaien",
+        getOff = "Skateboard: Afstappen",
+        forward = "Skateboard: Vooruit",
+        backward = "Skateboard: Achteruit",
+        left = "Skateboard: Links",
+        right = "Skateboard: Rechts",
+        jump = "Skateboard: Springen",
+    },
+    menus = {
+        holdup = "Vasthouden",
+        header = "Toon je board",
+    },
+    targets = {
+        getOn = "Opstappen",
+        pickUp = "Oppakken",
+        showOff = "Tonen",
+        hold = "Vasthouden",
+    },
+}
